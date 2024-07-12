371d2f51ab11ac22e42214d6e175cdda2ea73641
Zacharys Developer <Developer@Zacharys-MacBook-Pro-2.local>
2024-07-12 13:45:27 -0500

    autograder

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -15,7 +15,6 @@ const oldPointStructure = {
 function oldScrabbleScorer(word) {
 	word = word.toUpperCase();
 	let letterPoints = "";
-   // console.log(`\nEnter a word to score: ${word}`); //added
 
 	for (let i = 0; i < word.length; i++) {
       
@@ -37,35 +36,83 @@ function oldScrabbleScorer(word) {
 function initialPrompt(word) {
 
    console.log("Let's play some scrabble!"); //added
-   word = input.question(`\nEnter a word to score:`) //added
+   word = input.question(`\nEnter a word to score: `) //added
 
    let scoredword = oldScrabbleScorer(word) //added
-   console.log(scoredword);
+   // console.log(scoredword);
    
    return scoredword //modified
 };
 
-// console.log(initialPrompt());
-
-
 
 let newPointStructure;
 
-let simpleScorer;
+let simpleScorer = function(word) {
+   let score = 0
+   for (let i = 0; i < word.length; i++) {
+      score += 1;
+   }
+   return score;
+};
+// console.log(simpleScorer("Astrix"))
 
-let vowelBonusScorer;
+let vowelBonusScorer = function(word) {
+   let score = 0;
+   const vowels = [ 'A','E','I','O','U' ];
+   for (let i = 0; i < word.length; i ++) {
+      let letter = word[i];
+      if (vowels.includes(letter)) {
+         score += 3;
+      } else {
+            score += 1;
+         }
+    }
+    return score;
+}
+
+// vowelBonusScorer("babe")
+// console.log(vowelBonusScorer("Hello"));
 
 let scrabbleScorer;
 
-const scoringAlgorithms = [];
+const scoringAlgorithms = [
+   {"name": "Simple Scorer", 
+   "description": "Each letter is worth 1 point.",
+   "scorerFunction": simpleScorer},
 
-function scorerPrompt() {}
+   {"name": "Bonus Vowels", 
+   "description": "Vowels are 3 pts, consonants are 1 point",
+   "scorerFunction": vowelBonusScorer},
+
+   {"name": "Scrabble",
+   "description": "The Traditional scoring algorithm",
+   "scorerFunction": oldScrabbleScorer} //anonymous function based off variable - drop the ()
+];
+
+function scorerPrompt(response) {
+
+   for (let i = 0; scoringAlgorithms.length; i++){
+      let response = input.question("Which scoring algorithm would you like to use? "); 
+         if (i===0){
+            console.log(scoringAlgorithms.simpleScorer[0]);
+         } else if (i===1) {
+            console.log(scoringAlgorithms.vowelBonusScorer); 
+         } else (i===2) 
+            console.log(scoringAlgorithms.oldScrabbleScorer);
+
+      }
+      return response; //?
+
+ }
+ 
+ 
+
 
 function transform() {};
 
 function runProgram() {
    initialPrompt();
-   
+   scorerPrompt();
 }
 
 // Don't write any code below this line //
